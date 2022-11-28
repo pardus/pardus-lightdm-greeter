77c407c3327e59813da5be14f82d162c2b652a35
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-28 12:27:15 +0300

    config load fail exception

--- src/util.py
+++ src/util.py
@@ -18,13 +18,16 @@ def asynchronous(func):
 
 
 
-cfgs = ["/etc/pardus/greeter.conf"]
-if os.path.isdir("/etc/pardus/greeter.conf.d/"):
-    for cdir in os.listdir("/etc/pardus/greeter.conf.d/"):
-        cfgs.append("/etc/pardus/greeter.conf.d/"+cdir)
+try:
+    cfgs = ["/etc/pardus/greeter.conf"]
+    if os.path.isdir("/etc/pardus/greeter.conf.d/"):
+        for cdir in os.listdir("/etc/pardus/greeter.conf.d/"):
+            cfgs.append("/etc/pardus/greeter.conf.d/"+cdir)
 
-config = configparser.RawConfigParser()
-config.read(cfgs)
+    config = configparser.RawConfigParser()
+    config.read(cfgs)
+except:
+    config = []
 
 
 def get(variable, default=None, section="pardus"):
