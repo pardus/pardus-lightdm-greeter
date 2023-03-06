8cd82f0ae2255e8c033738ec5c2a4e9ec2b8ccf3
aliriza <aliriza.keskin@pardus.org.tr>
2023-03-06 13:12:40 +0000

    popdown power menu when button pressed

--- src/module/options.py
+++ src/module/options.py
@@ -15,19 +15,21 @@ def _network_button_event(widget):
 
 def _poweroff_event(widget):
     loginwindow.o("ui_stack_main").set_visible_child_name("page_poweroff")
+    loginwindow.o("ui_popover_powermenu").popdown()
+
 
 
 def _restart_event(widget):
     loginwindow.o("ui_stack_main").set_visible_child_name("page_restart")
-
+    loginwindow.o("ui_popover_powermenu").popdown()
 
 def _sleep_event(widget):
     loginwindow.o("ui_stack_main").set_visible_child_name("page_sleep")
-
+    loginwindow.o("ui_popover_powermenu").popdown()
 
 def sleep_action(widget):
     _cancel_event(widget)
-    GLib.timeout_add(500,lightdm.sleep)
+    GLib.timeout_add(600,lightdm.sleep)
 
 def _cancel_event(widget):
     loginwindow.err_handler()
