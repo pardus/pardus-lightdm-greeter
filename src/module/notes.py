d412c267cad4838aedf8e10a48702743dea97505
aliriza <aliriza.keskin@pardus.org.tr>
2024-02-01 19:00:59 +0300

    hide message icon when message length is 0 and not editable

--- src/module/notes.py
+++ src/module/notes.py
@@ -17,11 +17,21 @@ def module_init():
     loginwindow.o("ui_popover_notes").set_size_request(width/3, height/3)
     if get("text-file","","notes") != "":
         with open(get("text-file","","notes"),"r") as f:
-            loginwindow.o("ui_textview_notes").get_buffer().set_text(f.read())
+            content = f.read().strip()
+            if len(content) > 0:
+                loginwindow.o("ui_textview_notes").get_buffer().set_text(content)
+            else:
+                loginwindow.o("ui_button_note").hide()
+                return
         loginwindow.o("ui_textview_notes").set_editable(False)
         loginwindow.o("ui_textview_notes").set_can_focus(False)
         return
-    loginwindow.o("ui_textview_notes").get_buffer().set_text(gsettings_get("note-text"))
-    loginwindow.o("ui_textview_notes").set_can_focus(get("editable", True, "notes"))
-    loginwindow.o("ui_textview_notes").set_editable(get("editable", True, "notes"))
+    content = gsettings_get("note-text").strip()
+    editable = get("editable", True, "notes")
+    if not editable and len(content) == 0:
+        loginwindow.o("ui_button_note").hide()
+        return
+    loginwindow.o("ui_textview_notes").get_buffer().set_text(content)
+    loginwindow.o("ui_textview_notes").set_can_focus(editable)
+    loginwindow.o("ui_textview_notes").set_editable(editable)
     loginwindow.o("ui_textview_notes").get_buffer().connect("changed", _save_note_text)
