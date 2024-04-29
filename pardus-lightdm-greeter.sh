4919104796b9af613193da289c2f5ca6cce9281a
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-04-29 10:59:35 +0300

    crash log added. Users will see if greeter crashed

--- pardus-lightdm-greeter.sh
+++ pardus-lightdm-greeter.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-exec python3 /usr/share/pardus/pardus-lightdm-greeter/main.py 1>&2
+set -o pipefail
+python3 /usr/share/pardus/pardus-lightdm-greeter/main.py  |& tee ~/pardus-lightdm-greeter.log
+if [ $? -ne 0  ] ; then
+    python3 <<EOF
+import gi, os
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+w = Gtk.Window()
+txt=open(os.environ["HOME"]+"/pardus-lightdm-greeter.log", "r").read()
+l=Gtk.Label(txt)
+w.add(l)
+w.show_all()
+Gtk.main()
+EOF
+fi
