9298581aef62769f57297489d6024ad18566f51d
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-06-11 10:06:27 +0300

    fix daemon reload on read crash issue

--- src/module/daemon.py
+++ src/module/daemon.py
@@ -8,18 +8,21 @@ def module_init():
         if os.path.exists("/{}/pardus-greeter".format(busdir)):
             os.unlink("/{}/pardus-greeter".format(busdir))
         os.mkfifo("/{}/pardus-greeter".format(busdir))
-        f = open("/{}/pardus-greeter".format(busdir),"r")
-        username=""
-        password=""
-        data=json.loads(f.read())
-        if "username" in data:
-            username=data["username"]
-        if "password" in data:
-            password = data["password"]
-        if "session" in data:
-            lightdm.set(session = data["session"])
-        loginwindow.o("ui_entry_username").set_text(username)
-        loginwindow.o("ui_entry_password").set_text(password)
-        lightdm.set(username, password)
-        lightdm.login()
+        try:
+            with open("/{}/pardus-greeter".format(busdir),"r") as f:
+                username=""
+                password=""
+                data=json.loads(f.read())
+                if "username" in data:
+                    username=str(data["username"])
+                if "password" in data:
+                    password = str(data["password"])
+                if "session" in data:
+                    lightdm.set(session = str(data["session"]))
+                loginwindow.o("ui_entry_username").set_text(username)
+                loginwindow.o("ui_entry_password").set_text(password)
+                lightdm.set(username, password)
+                lightdm.login()
 
+        except Exception as e:
+           print(traceback.format_exc(), file=sys.stderr)
