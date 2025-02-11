2cf4577148ee884892a4ba226326065f785729d5
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-02-11 17:17:22 +0300

    cli function exit mechanism fix

--- src/cli.py
+++ src/cli.py
@@ -17,7 +17,7 @@ def login(username=None, password=None, session=None):
     """Login function"""
     if not os.path.exists("/var/lib/lightdm/pardus-greeter"):
         print("Failed to connect pardus lightdm greeter")
-        sys.exit(2)
+        return 2
     data = {}
     data["username"] = str(username)
     data["password"] = str(password)
@@ -28,27 +28,28 @@ def login(username=None, password=None, session=None):
         print(json.dumps(data))
         f.write(json.dumps(data))
         f.flush()
+    return 0
 
 def send_message(message=None):
     """Print message function"""
     if not os.path.exists("/var/lib/lightdm/pardus-greeter"):
         print("Failed to connect pardus lightdm greeter")
-        sys.exit(2)
+        return 2
     data = {}
     data["message"] = str(message)
     with open("/var/lib/lightdm/pardus-greeter", "a") as f:
         print(json.dumps(data))
         f.write(json.dumps(data))
         f.flush()
+    return 0
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: pardus-login [username] [password]", file=sys.stderr)
         sys.exit(1)
     if sys.argv[1] == "message":
-        send_message(sys.argv[2])
-        sys.exit(0)
+        sys.exit(send_message(sys.argv[2]))
     session = None
     if len(sys.argv) > 3:
         session = sys.argv[3]
-    login(sys.argv[1], sys.argv[2], session)
+    sys.exit(login(sys.argv[1], sys.argv[2], session))
